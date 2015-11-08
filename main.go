8dd2fff0d37bf5dfe2340ac24439411f615fcf0a
Bjørn Fjukstad <bjorn@cs.uit.no>
2015-11-08 16:48:46 +0100

    first version of scratch hall of fame

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"text/template"
+
+	"github.com/fjukstad/scratch"
+)
+
+func main() {
+	projects, err := GetProjects("ids")
+
+	f, err := os.OpenFile("index.html", os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	t, err := template.ParseFiles("index.template")
+	t.Execute(f, projects)
+
+	fmt.Println("index.html written succesfully")
+}
+
+func GetProjects(filename string) ([]*scratch.Project, error) {
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	projects := []*scratch.Project{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		id := scanner.Text()
+		p, err := scratch.GetProject(id)
+		if err != nil {
+			return nil, err
+		}
+		projects = append(projects, p)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return projects, nil
+}
